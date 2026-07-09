17706695c27a951998c83e08993b9a004ebd8b1d
Amit Swain <amitkumarswain2006@gmail.com>
2026-07-09 20:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (45.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,9 +1,29 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) { 
+//         for(int i=0;i<n;i++){
+//             nums1[i+m]=nums2[i];
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }
+
+// optimal solution: 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) { 
-        for(int i=0;i<n;i++){
-            nums1[i+m]=nums2[i];
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] merged = new int[m];
+        for(int i=0;i<m;i++){
+            merged[i]=nums1[i];
         }
-        Arrays.sort(nums1);
+        int p1=0;
+        int p2=0;
+        for(int i=0;i<nums1.length;i++){
+            if(p2>=n || (p1<m && merged[p1]<=nums2[p2])){
+                nums1[i]=merged[p1];
+                p1++;
+            }else{
+                nums1[i]=nums2[p2];
+                p2++; 
+            }
+        }
     }
 }
-
