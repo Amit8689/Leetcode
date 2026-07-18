ed3589d1c4d78bdfc8eb085c13532a07ea94d6c6
Amit Swain <amitkumarswain2006@gmail.com>
2026-07-18 13:13:56 +0530

    Time: 2 ms (92.15%) | Memory: 47.6 MB (76.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,43 @@
+// here is optimal solution using 2 pointer approach
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int[] arr=new int[n];
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(nums[i] != 0){
-                arr[j]=nums[i];
-                j++;
+        int slow=0,fast=0;
+        while(fast<nums.length){
+            if(nums[fast] != 0){
+                nums[slow]=nums[fast];
+                slow++;
+                fast++;
+            }else{
+                fast++; 
             }
         }
-        while(j<n){
-            arr[j]=0;
-            j++;
-        }
-        j=0;
-        while(j<n){
-            nums[j]=arr[j];
-            j++;
+        while(slow<nums.length){
+            nums[slow]=0;
+            slow++;
         }
     }
 }
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int n=nums.length;
+//         int[] arr=new int[n];
+//         int j=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i] != 0){
+//                 arr[j]=nums[i];
+//                 j++;
+//             }
+//         }
+//         while(j<n){
+//             arr[j]=0;
+//             j++;
+//         }
+//         j=0;
+//         while(j<n){
+//             nums[j]=arr[j];
+//             j++;
+//         }
+//     }
+// }
+
